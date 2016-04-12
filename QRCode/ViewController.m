9dd185900e836a1595baded4cf09ad56c29607a3
王文科 <576873656@qq.com>
2016-04-12 16:07:58 +0800

    Initial Commit

---
+++ QRCode/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  QRCode
+//
+//  Created by wangwenke on 16/4/12.
+//  Copyright © 2016年 wangwenke. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
